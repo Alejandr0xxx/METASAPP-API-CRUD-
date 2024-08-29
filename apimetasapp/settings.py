6faab1e9234c443eb9eb4a1e4ce1db65b35392ea
Alejandr0x <ytalejandrovevo@gmail.com>
2024-08-28 22:18:03 -0500

    Aparentemente parece que hemos terminado

--- apimetasapp/settings.py
+++ apimetasapp/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'apimetasapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        # 'NAME': 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
     }
 }
 
--- objectives/models.py
+++ objectives/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Objectives(models.Model):
+    FREQUENCY_STATUS = [
+        ('Daily', 'Daily'),
+        ('Weekly', 'Weekly'),
+        ('Monthly', 'Monthly'),
+        ('Yearly', 'Yearly'),
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.IntegerField(null=True)
+    details = models.TextField()
+    events = models.IntegerField()
+    icon = models.TextField(null=True)
+    objective_num = models.IntegerField(null=True)
+    deadline = models.DateField()
+    completed_times = models.IntegerField(default=0)
+    completed_times_bef = models.IntegerField(default=0)
+    is_completed_bef = models.BooleanField(default=False)
+    frequency = models.CharField(max_length=7,choices=FREQUENCY_STATUS)
+
+    class Meta:
+        db_table = 'objectives'
